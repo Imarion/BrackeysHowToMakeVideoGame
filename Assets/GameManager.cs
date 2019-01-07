ff0e01d9a8cbd4b03122b678f592add47a0c2538
emr <renquin@awtce.be>
2019-01-07 17:34:48 +0100

    Whole tutorial

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public float restartDelay = 1f;
+	public GameObject completeLevelUI;
+
+	bool gameHasEnded = false;
+
+	public void CompleteLevel() {
+		completeLevelUI.SetActive (true);
+	}
+
+	public void EndGame() {
+		if (gameHasEnded == false) {
+			gameHasEnded = true;
+			Debug.Log ("Game over");
+			Invoke("Restart", restartDelay);
+		}
+	}
+
+	void Restart() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+}
